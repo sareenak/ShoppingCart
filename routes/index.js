a3c21faf7b35ba93f67e390ea82b2fe0d8372a46
sareenak <43492259+sareenak@users.noreply.github.com>
2021-12-20 12:02:07 +0400

    Created an array in index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let Product=[
+    {
+      name:"IPhone 13 Pro Max",
+      category:"Phone",
+      image:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-pro-max-gold-select?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1631652956000",
+      Description:"A dramatically more powerful camera"
+    },
+    {
+      name:"IPhone 13 Pro Max",
+      category:"Phone",
+      image:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-pro-max-gold-select?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1631652956000",
+      Description:"A dramatically more powerful camera"
+
+    },
+    {
+      name:"IPhone 13 Pro Max",
+      category:"Phone",
+      image:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-pro-max-gold-select?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1631652956000",
+      Description:"A dramatically more powerful camera"
+    },
+    {
+      name:"IPhone 13 Pro Max",
+      category:"Phone",
+      image:"https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-pro-max-gold-select?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1631652956000",
+      Description:"A dramatically more powerful camera"
+    }
+  ]
+  res.render('index', {Product });
 });
 
 module.exports = router;
