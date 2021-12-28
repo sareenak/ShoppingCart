c29200a277ed5a41abf1d12473fa2db09a69638a
sareenak <43492259+sareenak@users.noreply.github.com>
2021-12-28 11:06:56 +0400

    user view page set

--- routes/user.js
+++ routes/user.js
@@ -1,30 +1,13 @@
 var express = require('express');
+const { helpers } = require('handlebars');
 var router = express.Router();
+var producthelpers=require('../helpers/product-helpers')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  producthelpers.getAllProducts().then((product)=>{
 
-  let products=[
-    {
-      name: "iPhone 13 Pro Max",
-      category: "Mobile",
-      description: "Example",
-      image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-pro-max-gold-select?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1631652956000"
-    },
-    {
-      name: "iPhone 13 Pro",
-      category: "Mobile",
-      description: "Example",
-      image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-pro-silver-select?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1631652954000"
-    },
-    {
-      name: "iPhone 13 Mini ",
-      category: "Mobile",
-      description: "Example",
-      image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-13-mini-blue-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1629842711000"
-    }
-  ]
-  res.render('index', {products,admin:false});
+  res.render('user/view-products', {product,admin:false})})
 });
 
 module.exports = router;
